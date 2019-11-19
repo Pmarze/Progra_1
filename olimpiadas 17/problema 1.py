69c8018a4577b37c2b24ede0a61e77b571eef35f
Pmarze <53276103+Pmarze@users.noreply.github.com>
2019-11-19 11:53:20 -0600

    Update problema 1.py
    terminado

--- olimpiadas 17/problema 1.py
+++ olimpiadas 17/problema 1.py
@@ -18,21 +18,24 @@ def diagonal(A):            #el parámetro es una lista
         if A[3-b,b]=='T':
             DIAGX[1]+=1
             DIAGY[1]+=1
-    return DIAGX, DIAGY
+    b,c=max(DIAGX),max(DIAGY)
+    return b,c
 def filas(B):
     filX=[0,0,0,0]
     filO=[0,0,0,0]
     for a in range(4):
         filX[a]=np.count_nonzero(B[:,a]=='X')+np.count_nonzero(B[:,a]=='T')
         filO[a]=np.count_nonzero(B[:,a]=='O')+np.count_nonzero(B[:,a]=='T')
-    return filX, filO
+    b,c=max(filX),max(filO)
+    return b,c
 def columnas(B):
     colX=[0,0,0,0]
     colO=[0,0,0,0]
     for a in range(4):
         colX[a]=np.count_nonzero(B[a,:]=='X')+np.count_nonzero(B[a,:]=='T')
         colO[a]=np.count_nonzero(B[a,:]=='O')+np.count_nonzero(B[a,:]=='T')
-    return colX, colO
+    b,c=max(colX),max(colO)
+    return b,c
 def puntos(B):
     a = np.count_nonzero( B =='.')
     return a
@@ -42,7 +45,38 @@ for a in range(0,T):
     for b in range(4):
         A.append(list(input()))
     B=np.array(A)
-    C=columnas(B)
-    D=filas(B)
-    E=puntos(B)
-    print(C,D,E)
+    A = []
+    C=filas(B)
+    D=columnas(B)
+    E=diagonal(B)
+    F=puntos(B)
+    #print(C,D,E,F)
+    G=max([C[0],D[0],E[0]])
+    H=max([C[1],D[1],E[1]])
+    #print(G,H)
+    if F==0:            #El juego ya terminó
+        if G==4:
+            if H==4:
+                print('DRAW')
+            else:
+                print('Ganó X')
+        elif H==4:
+            if G==4:
+                print('DRAW')
+            else:
+                print('Ganó O')
+        else:
+            print('DRAW')
+    elif F!=0:
+        if G==4:
+            if H==4:
+                print('DRAW')
+            else:
+                print('Ganó X')
+        elif H==4:
+            if G==4:
+                print('DRAW')
+            else:
+                print('Ganó O')
+        else:
+            print('El juego no ha terminado')
