f24fa3faf8da3c076a1ea370928abcaed472071c
Pmarze <53276103+Pmarze@users.noreply.github.com>
2019-11-18 21:08:19 -0600

    Update problema 2.py
    Completado

--- Tarea2/olimpiadas 17/problema 2.py
+++ Tarea2/olimpiadas 17/problema 2.py
@@ -6,20 +6,17 @@ def palindromo(c):           #el parámetro es un número
     P=[str(i) for i in P]
     a=int(''.join(P))
     return abs(a-c)
-
-C=[]
 T=int(input())
 for a in range(0,T):
     A, B = map(int, input().strip('\n').split(' ')) #A-inicio      B-final
-    start = time.time()
+    #start = time.time()
+    z=0
     for b in range(0,int(math.sqrt(B))+1):          #desde cero hasta raíz de B, porque no hay cuadrados más grandes
-        if palindromo(b)==0:
-            c=b**2
-            if (c)>=A:                                  #descartar los cuadrados menores al rango
-                if palindromo(c)==0:
-                    C.append(c)
-    end = time.time()
-    print(C)
-    print(end-start, 'segundos')
-
-    C=[]
+        if palindromo(b)==0:                        #Analizar si el número es un palíndromo
+            c=b**2                                  # Si es palindromo elevar al cuadrado para ver si su cuadrado también es palindromo
+            if (c)>=A:                              #descartar los cuadrados menores al rango
+                if palindromo(c)==0:                #Analizar si el número es un palíndromo
+                    z+=1
+    #end = time.time()
+    print(z)
+    #print(end-start, 'segundos')
