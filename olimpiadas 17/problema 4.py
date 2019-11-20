eb1b08c72acf69ad887847ce2ef878cacd70431c
Pmarze <53276103+Pmarze@users.noreply.github.com>
2019-11-19 18:37:50 -0600

    Create problema 4.py

---
+++ olimpiadas 17/problema 4.py
@@ -0,0 +1,31 @@
+def suma(C,N):
+    A=0
+    for a in range(N):
+        A=C[a]+A
+        if A>K:
+            A=A-C[a]
+            break
+    return A,a
+def corrimiento(C,n):
+    D=C.copy()
+    for a in range(len(C)):
+        D[a-n]=C[a]  # n es el corrimiento de espacios
+    return D
+def sumar(D):
+
+A=0
+C=[]
+F=[]
+T=int(input())
+for a in range(T):
+    R,K,N=map(int, input().strip('\n').split(' '))  #K= capacidad del vagón, R veces repite
+    C=input().strip('\n').split(' ')
+    C=[int(i) for i in C]
+    #print(C)
+    D=C.copy()
+    for b in range(R):
+        E=suma(D,N)
+        F.append(E[0])
+        D=corrimiento(D,E[1])
+    print(F)
+    E,F,D=[],[],[]
