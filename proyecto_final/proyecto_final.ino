1bbc45a051f63290141f84428baf9b4c91c39d4b
Pmarze <53276103+Pmarze@users.noreply.github.com>
2019-11-29 09:08:27 -0600

    Update proyecto_final.ino

--- proyecto_final/proyecto_final.ino
+++ proyecto_final/proyecto_final.ino
@@ -12,6 +12,8 @@ int a=0;                     //VARIABLE PARA LEER PAM
 int b=0;                     //VARIABLE PARA LEER PAZ
 int c=0;                     //VARIABLE PARA LEER PVE
 int d=0;                     //VARIABLE PARA LEER PRO
+int e=0;                     //VARIABLE PARA ENTRADA
+int lose=0;
 boolean estado= true;        //VARIABLE BOOLEANA DE ESTADO DEL JUEGO TRUE=AÚN EN JUEGO FALSE=TERMINADO
 int nivel=1;
 int lista[50];
@@ -31,21 +33,14 @@ void setup(){
 }
 
 void loop(){
-  
-  secuencia();
   ganar();
-  nivel+=1;
+  secuencia();
+  entrada();  
   
-  
-  while (estado==true){
-    a=digitalRead(PAM);        //LEER EL ESTADO DE PAM
-    b=digitalRead(PAZ);        //LEER EL ESTADO DE PAZ
-    c=digitalRead(PVE);        //LEER EL ESTADO DE PVE
-    d=digitalRead(PRO);        //LEER EL ESTADO DE PRO
-    estado=false;  
-   
-  }
-  
+  if (lose==1);
+    nivel=1;
+    aleatorio();
+  estado=true; 
 }
 
 void amarillo(){             //FUNCIÓN DEL LED AMARILLO
@@ -86,9 +81,9 @@ void aleatorio(){            //GENERA UNA LISTA DE 50 ELEMENTOS ALEATORIOS
   lista[i]=random(1,5);  
   }
   }
-void secuencia(){
-  for (int i=0;i< nivel;i++){
-    if (lista[i]==1){
+void secuencia(){           //FUNCIÓN PARA MOSTRAR LA SECUENCIA DESDE 1 HASTA EL NIVEL ACTUAL
+  for (int i=0;i< nivel;i++){ //AL TENER VALORES ENTRE 0 Y 4 SOLO TENEMOS 4 POSIBLES CASOS
+    if (lista[i]==1){       //SI EL ELEMENTO i DE LA LISTA ES UN 1, SE ENCIENDE EL LED AMARILLO
       amarillo();
       }
     if (lista[i]==2){
@@ -102,6 +97,73 @@ void secuencia(){
       } 
   }
   }
+void entrada(){
+  e=0;
+  while (estado==true){
+    while (e<=nivel){
+    a=digitalRead(PAM);        //LEER EL ESTADO DE PAM
+    b=digitalRead(PAZ);        //LEER EL ESTADO DE PAZ
+    c=digitalRead(PVE);        //LEER EL ESTADO DE PVE
+    d=digitalRead(PRO);        //LEER EL ESTADO DE PRO
+      Serial.println(e);
+      if (e==nivel+1){
+        estado=false;
+        
+        break;
+        }
+      if (a==LOW){
+        if (lista[e]==1){
+          amarillo();
+          e+=1;
+          }
+        else{
+          estado=false;
+          perder();
+          lose=1;
+          break;
+          }
+        }  
+      if (b==LOW){
+        if (lista[e]==2){
+          azul();
+          e+=1;
+          }
+        else{
+          estado=false;
+          perder();
+          lose=1;
+          break;
+          }
+        }  
+      if (c==LOW){
+        if (lista[e]==3){
+          verde();
+          e+=1;
+          }
+        else{
+          estado=false;
+          perder();
+          lose=1;
+          break;
+          }
+        }  
+      if (d==LOW){
+        if (lista[e]==4){
+          rojo();
+          e+=1;
+          }
+        else{
+          estado=false;
+          perder();
+          lose=1;
+          break;
+          }
+        }  
+    }
+  }
+  nivel+=1;
+}
+  
 void ganar(){
   for (int a=0;a<5;a++){
   digitalWrite(AM,HIGH);
@@ -115,3 +177,18 @@ void ganar(){
   digitalWrite(RO,LOW);
   delay(75);
 }}
+void perder(){
+  for (int b=0;b<5;b++){
+  digitalWrite(AM,HIGH);
+  digitalWrite(AZ,LOW);
+  digitalWrite(VE,LOW);
+  digitalWrite(RO,HIGH);
+  delay(75);
+  digitalWrite(AM,LOW);
+  digitalWrite(AZ,HIGH);
+  digitalWrite(VE,HIGH);
+  digitalWrite(RO,LOW);
+  delay(75);
+  digitalWrite(AZ,LOW);
+  digitalWrite(VE,LOW);
+  }}
